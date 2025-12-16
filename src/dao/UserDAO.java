e6b6fa354604c263c55aa148303cda142ff72c52
Intro-iu <Intro-iu@outlook.com>
2025-12-16 14:47:35 +0800

    feat: implement UserDAO and UserService

---
+++ src/dao/UserDAO.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.User;
+import util.DBUtil;
+
+public class UserDAO {
+
+    /**
+     * Authenticate user
+     * @param username
+     * @param password
+     * @return User object if valid, null otherwise
+     */
+    public User login(String username, String password) {
+        String sql = "SELECT * FROM t_user WHERE username = ? AND password = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        User user = null;
+
+        try {
+            conn = DBUtil.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                user = new User();
+                user.setId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setNickname(rs.getString("nickname"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(conn, pstmt, rs);
+        }
+        return user;
+    }
+}
